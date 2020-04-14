306549b3e347d591ce7f952d9c986e38f9fe9d17
TristanMayne <61426351+TristanMayne@users.noreply.github.com>
2020-04-14 15:20:03 -0500

    More changes and updates, Express and Render

--- ExpressPractice/app.js
+++ ExpressPractice/app.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var app = express();
 
+app.get("/", function(req,res){
+    res.send("hello welcome to my project");
+});
+
 app.get("/repeat/:repeatstr/:number", function(req, res){
     var prntstr = "";
     var num = req.params.number;
@@ -12,13 +16,18 @@ app.get("/repeat/:repeatstr/:number", function(req, res){
 });
 
 app.get("/speak/:animal", function(req, res){
-    var animal = req.params.animal;
-    if (animal === "pig"){
-        res.send("Oink");
-    }
-    else if (animal === "cow"){
-        res.send("Moo");
-    }
+    var animal = req.params.animal.toLowerCase;
+    var sounds = {
+        pig: "Oink",
+        dog: "woof",
+        cat: "meow",
+    };
+    var sound = sounds[animal];
+    res.send("The " + animal + " says " + sound);
+});
+
+app.get("*", function(req, res){
+    res.send("what are you looking for?");
 });
 
 app.listen(3000, process.env.IP, function(){
